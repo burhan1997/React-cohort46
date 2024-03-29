e40f0007b466444024770f656d539c1355999d31
burhan1997 <burhanelaldi97@gmail.com>
2024-03-29 13:55:36 +0100

    week1 feedback is applied

--- week1/project/ecommerce/src/components/ProductList.js
+++ week1/project/ecommerce/src/components/ProductList.js
@@ -1,15 +1,18 @@
 import React from "react";
 
+// my purpose in this block is to filter the list of products based on a selected category. normalizeCategory function takes a category string as input and converts it to lowercase. It ensures consistency in the comparison of category strings.
 const ProductList = ({ products, selectedCategory }) => {
-  const filteredProducts = selectedCategory
-    ? products.filter(
-        (product) =>
-          product.category &&
-          product.category
-            .toLowerCase()
-            .includes(selectedCategory.toLowerCase().replace("fake: ", ""))
-      )
-    : products;
+  const normalizeCategory = (category = "") => category.toLowerCase();
+
+  const selectedCategoryNormalized = selectedCategory
+    ? selectedCategory.toLowerCase().replace("fake: ", "")
+    : "";
+
+  const filteredProducts = products.filter((product) => {
+    const productCategory = normalizeCategory(product.category);
+    return productCategory.includes(selectedCategoryNormalized);
+  });
+
 
   return (
     <div className="product-list">
